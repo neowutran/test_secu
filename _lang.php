0390832dd22c031892b16c2f94cbff45e60de5cd
neowutran <me@neowutran.ovh>
2016-06-10 12:00:14 +0200

    Update _lang.php

--- _lang.php
+++ _lang.php
@@ -1,7 +1,14 @@
 <?php 
 echo "test4";
+echo "======\n"
 print_r(get_defined_vars());
+echo "=======\n";
 $homepage = file_get_contents('index.php');
-echo $homepage.'printed';
+echo "======\n";
+var_dump($homepage);
+echo htmlentities($homepage);
+echo "=======\n";
 echo getcwd();
+echo "======\n";
 print_r(scandir("./"));
+echo "======\n";
