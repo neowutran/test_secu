c14c6456825655ee355c07c814fecef4f0faf7e3
neowutran <me@neowutran.ovh>
2016-06-10 11:57:18 +0200

    Update _lang.php

--- _lang.php
+++ _lang.php
@@ -2,7 +2,6 @@
 echo "test4";
 print_r(get_defined_vars());
 $homepage = file_get_contents('index.php');
-echo $homepage;
+echo $homepage.'printed';
 echo getcwd();
 print_r(scandir("./"));
-highlight_file("index.php");
